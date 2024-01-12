78dce60c5def57ed73a654d44e29e7791c912825
Diriba <39425889+Diriba1@users.noreply.github.com>
2024-01-12 15:38:23 +0300

    created view.py file

--- algo_backend_django/mainapp/views.py
+++ algo_backend_django/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Account
+
+
+def index(request):
+    """Display all the created standalone accounts."""
+
+    accounts = Account.objects.order_by("-created")
+    context = {"accounts": accounts}
+    return render(request, "mainapp/index.html", context)
